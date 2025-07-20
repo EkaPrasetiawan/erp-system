e8c017cafced0745656a1994f9279e424518483b
EkaPrasetiawan <prasetyawan976@gmail.com>
2025-07-21 00:26:37 +0700

    membuat file ada folder sales

---
+++ assets/nav.php
@@ -0,0 +1,39 @@
+<div class="sb-sidenav-menu">
+    <div class="nav">
+        <div class="sb-sidenav-menu-heading">Utama</div>
+        <a class="nav-link" href="home.php">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-house"></i></div>
+            Home
+        </a>
+        <a class="nav-link" href="sales.php">
+            <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+            Sales
+        </a>
+        <div class="sb-sidenav-menu-heading">Work</div>
+        <a class="nav-link" href="rombongan.php">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-people-pulling"></i></div>
+            Rombongan
+        </a>
+        <a class="nav-link" href="budgeting.php">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-file-invoice-dollar"></i></div>
+            Budgeting
+        </a>
+        <div class="sb-sidenav-menu-heading">Fasiliti</div>
+        <a class="nav-link" href="fasilitas.php">
+            <div class="sb-nav-link-icon"><i class="fas fa-city"></i></div>
+            Fasilitas WK
+        </a>
+        <a class="nav-link" href="vendor-fasilitas.php">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-up-right-from-square"></i></div>
+            Vendor
+        </a>
+        <a class="nav-link" href="f&b.php">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-utensils"></i></div>
+            F&B
+        </a>
+    </div>
+</div>
+<div class="sb-sidenav-footer">
+    <div class="small">Logged in as:</div>
+    Start Bootstrap
+</div>
